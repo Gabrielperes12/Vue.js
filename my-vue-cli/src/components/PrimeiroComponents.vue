cc296cb68bdfcc0b0da8820b83a51d0431510fea
Gabriel Peres <114517457+Gabrielperes12@users.noreply.github.com>
2022-11-29 00:23:09 -0300

    aula 06

--- my-vue-cli/src/components/PrimeiroComponents.vue
+++ my-vue-cli/src/components/PrimeiroComponents.vue
@@ -1,9 +1,19 @@
 <template>
+<div>
     <h1>Olá VUE!</h1>
+    <p> Meu nome é {{nome}} tenho {{idade}} e trabalho como {{profissão}}</p>
+</div>
 </template>
 
 <script>
     export default{
-       name:'PrimeiroComponents'
+       name:'PrimeiroComponents',
+       data(){
+        return{
+            nome: 'Gabriel',
+            idade: 22,
+            profissão: 'Vg'
+        }
+       }
     }    
 </script>
