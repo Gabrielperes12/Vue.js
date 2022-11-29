f877f6b6c59d1cc5aee6acaefd2f655a87fe5d4a
Gabriel Peres <114517457+Gabrielperes12@users.noreply.github.com>
2022-11-29 00:56:13 -0300

    aula 07

--- my-vue-cli/src/App.vue
+++ my-vue-cli/src/App.vue
@@ -1,13 +1,19 @@
 <template>
+  <div>
   <PrimeiroComponents/>
+  <LifeCycle/>
+  </div>
+
 </template>
 
 <script>
     import PrimeiroComponents from './components/PrimeiroComponents.vue'
+    import LifeCycle from './components/LifeCycle.vue'
     
     export default{
       components:{
-        PrimeiroComponents
+        PrimeiroComponents,
+        LifeCycle
       }
     }
 </script>
---
+++ my-vue-cli/src/components/LifeCycle.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Meu nome: {{nome}}</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'LifeCycle',
+        data() {
+            return {
+                nome: 'Ainda não sei'
+            }
+        },
+        created(){
+            setTimeout(() => {
+                this.nome = 'Matheus'
+            }, 1000  );
+
+          
+        },
+        mounted(){
+            setTimeout(() => {
+                this.nome = 'Renan'
+            }, 2000);
+        }
+    }
+</script>
