6dcea78f0589e3214c6d89979f48fe9e548c13e5
Gabriel Peres <114517457+Gabrielperes12@users.noreply.github.com>
2022-12-06 20:30:38 -0300

    aula 15

---
+++ my-vue-cli/src/components/MultiplosEventos.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button @click="primeiro()">Ativar Multiplos Eventos</button>
+        <p>{{multiplos_eventos}}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'MultiplosEventoS',
+        components:{
+
+        },
+        data(){
+            return{
+                multiplos_eventos:''
+            }
+        },
+        methods:{
+           primeiro(){
+            this.multiplos_eventos = 'Primeiro evento!'
+           }
+        }
+
+    }
+</script>
+
+<style scoped>
+    div{
+        margin: 100px;
+    }
+</style>
--- my-vue-cli/src/components/PrimeiroComponents.vue
+++ my-vue-cli/src/components/PrimeiroComponents.vue
@@ -2,12 +2,17 @@
 <div>
     <h1>Olá VUE!</h1>
     <p> Meu nome é {{nome}} tenho {{idade}} e trabalho como {{profissão}}</p>
+    <MultiplosEventos/>
 </div>
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue'
     export default{
-       name:'PrimeiroComponents',
+       name:'PrimeiroComponentS',
+       components:{
+        MultiplosEventos
+       },
        data(){
         return{
             nome: 'Gabriel',
