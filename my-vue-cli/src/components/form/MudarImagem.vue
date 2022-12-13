96b57a3b5e33a78cf80646daed4cf4168b8c3219
Gabriel Peres <114517457+Gabrielperes12@users.noreply.github.com>
2022-12-13 00:56:12 -0300

    aula 18

---
+++ my-vue-cli/src/components/form/MudarImagem.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <button @click="$emit('mudarImagem')"> Trocar imagem de perfil</button>
+    </div>
+</template>
+<script>
+    export default{
+        name:'MudarImageM',
+        emits: ['mudarImagem ']
+    }
+</script>
--- my-vue-cli/src/components/form/Picture.vue
+++ my-vue-cli/src/components/form/Picture.vue
@@ -1,16 +1,24 @@
 <template>
     <div>
         <img :src="avatar" alt="gabriel">
+        <MudarImagem @mudar-imagem="trocarImagem" />
     </div>
 </template>
 
 <script>
+import MudarImagem from './MudarImagem.vue';
     export default{
+  components: { MudarImagem },
         name:'PicturE',
             data(){
                 return{
                 avatar: '/img/avatar.png',
                 descrição: 'gabriel'}
+            },
+            methods:{
+                trocarImagem(){
+                    this.avatar = '/img/avatar2.png'
+                }
             }
     }
 </script>
